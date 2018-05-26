a3677e09fe2236510afee804af1a9d2759256653
ChiWeiHsiao <kiwi010379@gmail.com>
2018-05-26 19:42:03 +0200

    Load/Save optimizer and model states

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# %load main.py
 from params import *
 from model import DeepVO
 #from Dataloader_loss import *
@@ -10,35 +9,23 @@ from torch.autograd import Variable
 import torch.utils.data as Data
 from data_manager import prepare_sequence_data
 
-save_path = '{},ep{},b{},lr{}'.format(
-    params.solver,
-    params.epochs,
-    params.batch_size,
-    params.lr)
 
-print('torch.cuda.is_available(): ', torch.cuda.is_available())
-print('======================================================')
+M_deepvo = DeepVO(params.img_h, params.img_w)
 use_cuda = torch.cuda.is_available()
 
 # Load FlowNet weights pretrained with FlyingChairs
-if params.pretrained:
+if params.pretrained_flownet and params.load_model_path == None:
 	if use_cuda:
-		pretrained_w = torch.load(params.pretrained)
+		pretrained_w = torch.load(params.pretrained_flownet)
 	else:
-		pretrained_w = torch.load(params.pretrained, map_location='cpu')
-	print('load pretrained model')
-else:
-	pretrained_w = None
+		pretrained_w = torch.load(params.pretrained_flownet_flownet, map_location='cpu')
+	print('Load FlowNet pretrained model')
+	# Use only conv-layer-part of FlowNet as CNN for DeepVO
+	model_dict = M_deepvo.state_dict()
+	update_dict = {k: v for k, v in pretrained_w['state_dict'].items() if k in model_dict}
+	model_dict.update(update_dict)
+	M_deepvo.load_state_dict(model_dict)
 
-
-M_deepvo = DeepVO(params.img_h, params.img_w)#320, 96)
-
-
-# Use only conv-layer-part of FlowNet as CNN for DeepVO
-model_dict = M_deepvo.state_dict()
-update_dict = {k: v for k, v in pretrained_w['state_dict'].items() if k in model_dict}
-model_dict.update(update_dict)
-M_deepvo.load_state_dict(model_dict)
 if use_cuda:
     print('CUDA used.')
     M_deepvo.cuda()
@@ -93,24 +80,26 @@ for seq in seq_list:
     #DEEPVO TRAINING PROCESS
     
 '''
-
-
-
-
 # toy data
 #x = torch.randn(1, 3, 6, params.img_h, params.img_w).type(torch.FloatTensor))  # b_size, seq_len, channels(3*2andn(1, 3, 6).type(torch.FloatTensor)
 
+
 # Train
+M_deepvo.train()
+
 if params.solver == 'Adagrad':
 	optimizer = torch.optim.Adagrad(M_deepvo.parameters(), lr=params.lr)
 elif params.solver == 'Cosine':
 	optimizer = torch.optim.SGD(M_deepvo.parameters(), lr=params.lr)
-M_deepvo.train()
 
-record_name = params.record_name
-save_model_name = params.save_model_name
-print('Record loss in: ', record_name)
-print('Save model in: ', save_model_name)
+# Load trained model and optimizer
+if params.load_model_path:
+	M_deepvo.load_state_dict(torch.load(params.load_model_path))
+if params.load_optimzer_path:
+	optimizer.load_state_dict(torch.load(params.load_model_path)).
+	#optimizer.state = defaultdict(dict, optimizer.state)
+
+print('Record loss in: ', params.record_path)
 min_loss = 1e10
 
 for ep in range(params.epochs):
@@ -131,10 +120,10 @@ for ep in range(params.epochs):
 		loss_mean += l
 	loss_mean /= (it+1)
 
-	f = open(record_name, 'w') if ep == 0 else open(record_name, 'a')
+	f = open(params.record_path, 'w') if ep == 0 else open(params.record_path, 'a')
 	f.write('{}\n'.format(record_loss_list))
 	f.write('===== Epoch {} mean of loss: {} =====\n'.format(ep, loss_mean))
-	f.close()
+	
 	print('{}'.format(record_loss_list))
 	print('===== Epoch {} mean of loss: {} =====\n'.format(ep, loss_mean))
 
@@ -142,6 +131,8 @@ for ep in range(params.epochs):
 	if loss_mean < min_loss:
 		min_loss = loss_mean
 		print('Save model at ep {}, mean of loss: {}'.format(ep, loss_mean))
-		torch.save(M_deepvo.state_dict(), save_model_name)
-
+		f.write('Save model!\n')
+		torch.save(M_deepvo.state_dict(), params.save_model_path)
+		torch.save(optimizer.state_dict(), params.save_optimzer_path)
+	f.close()
 
--- params.py
+++ params.py
@@ -12,12 +12,16 @@ class Parameters():
 		self.batch_size = 16  # 64 8 128
 		self.epochs = 500
 		self.rnn_hidden_size = 1000  #1000 500
-		self.pretrained = './pretrained/flownets_bn_EPE2.459.pth.tar'
 
 		self.use_video = ['01', '07', '09']
 		self.load_data = ['KITTI/segmented_image/{}_seq_{}_{}_im_{}_{}.npz'.format(video, self.seq_len[0], self.seq_len[1], self.img_h, self.img_w) for video in self.use_video]
 
-		self.record_name = 'records/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
-		self.save_model_name = 'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		self.pretrained_flownet = './pretrained/flownets_bn_EPE2.459.pth.tar'  #'None'
+		self.load_model_path = None #'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.model'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		self.load_optimzer_path = None #'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.optimizer'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+
+		self.record_path = 'records/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		self.save_model_path = 'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.model'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		self.save_optimzer_path = 'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.optimizer'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
 
 params = Parameters()
