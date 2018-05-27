645e0e751e7e632282ee1a8fb6b50de245a68991
ChiWeiHsiao <kiwi010379@gmail.com>
2018-05-27 16:04:55 +0200

    Add optimizers

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from params import *
+from params import params
 from model import DeepVO
 #from Dataloader_loss import *
 
 import numpy as np
 import time
 import torch
-from torch.autograd import Variable
+
 import torch.utils.data as Data
 from data_manager import prepare_sequence_data
 
 
+# Write all hyperparameters to record_path
+p = vars(params)
+with open(params.record_path, 'a') as f:
+	f.write('\n'.join("%s: %s" % item for item in p.items()))
+	f.write('\n')
+
 M_deepvo = DeepVO(params.img_h, params.img_w)
 use_cuda = torch.cuda.is_available()
+if use_cuda:
+    print('CUDA used.')
+    M_deepvo.cuda()
+
 
 # Load FlowNet weights pretrained with FlyingChairs
 if params.pretrained_flownet and params.load_model_path == None:
@@ -26,27 +36,35 @@ if params.pretrained_flownet and params.load_model_path == None:
 	model_dict.update(update_dict)
 	M_deepvo.load_state_dict(model_dict)
 
-if use_cuda:
-    print('CUDA used.')
-    M_deepvo.cuda()
 
 # Prepare Data
-#X, Y = prepare_sequence_data(['07'], params.seq_len, 'single')
+#X, Y, seq_lengths = prepare_sequence_data(['07'], params.seq_len, 'single')
+use_pad = (params.seq_len[0] != params.seq_len[1])
 start_t = time.time()
 if len(params.load_data) == 1:
 	data = np.load(params.load_data[0])
 	X, Y = data['x'], data['y']
+	if use_pad:
+		seq_lengths = data['seq_lengths']
 elif len(params.load_data) > 1:
 	X, Y = [], []
+	seq_lengths = []
 	for i, d in enumerate(params.load_data):
 		data = np.load(d)
 		x, y = data['x'], data['y']
 		print('{}: x: {}, y: {}'.format(d, x.shape, y.shape))
 		X = x if i == 0 else np.concatenate((X, x), axis=0)
 		Y = y if i == 0 else np.concatenate((Y, y), axis=0)
+		if use_pad:
+			s = data['seq_lengths']
+			seq_lengths = s if i == 0 else np.concatenate((seq_lengths, s), axis=0) 
+
 
 print('Load data use {} sec'.format(time.time()-start_t))
 print('X: {}, Y: {}'.format(X.shape, Y.shape))
+if use_pad:
+	print('Seq_lengths: {}'.format(seq_lengths.shape))
+	seq_lengths = troch.from_numpy(seq_lengths)
 
 X, Y = torch.from_numpy(X), torch.from_numpy(Y)
 X = X.type(torch.FloatTensor)  # 0-255 is originally torch.uint8
@@ -54,10 +72,13 @@ print('max in tensor X:', X.max())
 
 # Preprocess, X subtract by the mean RGB values of training set
 for c in range(3):
-	mean = torch.mean(X[:, :, c])
-	X[:,:,c] -= mean
+	X[:,:,c] -= params.RGB_means[c]
+	#mean = torch.mean(X[:, :, c])
+	#print('mean: ', mean.numpy())
+	#X[:,:,c] -= mean
 
 train_dataset = Data.TensorDataset(X, Y)
+#train_dataset = Data.TensorDataset(X, Y, seq_lengths)  #AttributeError: 'list' object has no attribute 'size
 train_dl = Data.DataLoader(
     dataset=train_dataset,
     batch_size=params.batch_size,
@@ -83,25 +104,28 @@ for seq in seq_list:
 # toy data
 #x = torch.randn(1, 3, 6, params.img_h, params.img_w).type(torch.FloatTensor))  # b_size, seq_len, channels(3*2andn(1, 3, 6).type(torch.FloatTensor)
 
+if params.optim['opt'] == 'Adam':
+	optimizer = torch.optim.Adam(M_deepvo.parameters(), lr=0.001, betas=(0.9, 0.999))
+elif params.optim['opt'] == 'Adagrad':
+	optimizer = torch.optim.Adagrad(M_deepvo.parameters(), lr=params.optim['lr'])
+elif params.optim['opt'] == 'Cosine':
+	optimizer = torch.optim.SGD(M_deepvo.parameters(), lr=params.optim['lr'])
+	T_iter = params.optim['T']*len(train_dl)
+	lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_iter, eta_min=0, last_epoch=-1)
 
-# Train
-M_deepvo.train()
-
-if params.solver == 'Adagrad':
-	optimizer = torch.optim.Adagrad(M_deepvo.parameters(), lr=params.lr)
-elif params.solver == 'Cosine':
-	optimizer = torch.optim.SGD(M_deepvo.parameters(), lr=params.lr)
-
-# Load trained model and optimizer
-if params.load_model_path:
+if params.resume:
+	print('Load model')
 	M_deepvo.load_state_dict(torch.load(params.load_model_path))
-if params.load_optimzer_path:
-	optimizer.load_state_dict(torch.load(params.load_model_path)).
+	print('Load optimizer')
+	opt_state_dict = torch.load(params.load_optimzer_path)
+	optimizer.load_state_dict(opt_state_dict)
 	#optimizer.state = defaultdict(dict, optimizer.state)
 
-print('Record loss in: ', params.record_path)
-min_loss = 1e10
 
+print('Record loss in: ', params.record_path)
+min_loss = self.min_loss
+
+M_deepvo.train()
 for ep in range(params.epochs):
 	loss_mean = 0
 	record_loss_list = []
@@ -112,6 +136,8 @@ for ep in range(params.epochs):
 			batch_x = batch_x.cuda(non_blocking=True)
 		# Train
 		ls = M_deepvo.step(batch_x, batch_y, optimizer)
+		if params.optim == 'Cosine':
+			lr_scheduler.step()
 		record_loss_list.append(ls)
 
 	# Record mean loss of this epoch
@@ -120,7 +146,7 @@ for ep in range(params.epochs):
 		loss_mean += l
 	loss_mean /= (it+1)
 
-	f = open(params.record_path, 'w') if ep == 0 else open(params.record_path, 'a')
+	f = open(params.record_path, 'a')
 	f.write('{}\n'.format(record_loss_list))
 	f.write('===== Epoch {} mean of loss: {} =====\n'.format(ep, loss_mean))
 	
@@ -128,11 +154,10 @@ for ep in range(params.epochs):
 	print('===== Epoch {} mean of loss: {} =====\n'.format(ep, loss_mean))
 
 	# Save model
-	if loss_mean < min_loss:
+	if loss_mean < min_loss and ep % 2 == 0:
 		min_loss = loss_mean
-		print('Save model at ep {}, mean of loss: {}'.format(ep, loss_mean))
 		f.write('Save model!\n')
+		print('Save model at ep {}, mean of loss: {}'.format(ep, loss_mean))  # use 4.6 sec 
 		torch.save(M_deepvo.state_dict(), params.save_model_path)
 		torch.save(optimizer.state_dict(), params.save_optimzer_path)
 	f.close()
-
--- params.py
+++ params.py
@@ -2,26 +2,33 @@ __all__ = ['params', 'Parameters']
 
 class Parameters():
 	def __init__(self):
-		self.lr = 0.001
-		self.solver = 'Adagrad'  # Adagrad Cosine
+		self.optim = {'opt': 'Adam'}
+					# {'opt': 'Adagrad', 'lr': 0.01}
+					# {'opt': 'Adam'}
+					# {'opt': 'Cosine', 'T': 100 , 'lr': 0.001}
 
 		self.img_w = 608  # 608  613  1226 
 		self.img_h = 184  # 184  185  370 
-		self.seq_len = [8, 8]  # 11, 11
+		self.seq_len = [8, 8]  # [8, 10]
 
 		self.batch_size = 16  # 64 8 128
-		self.epochs = 500
+		self.epochs = 400
 		self.rnn_hidden_size = 1000  #1000 500
 
 		self.use_video = ['01', '07', '09']
+		self.RGB_means = [90.72659, 95.17316, 94.42595]  # caculated with videos 01+07+09
+
 		self.load_data = ['KITTI/segmented_image/{}_seq_{}_{}_im_{}_{}.npz'.format(video, self.seq_len[0], self.seq_len[1], self.img_h, self.img_w) for video in self.use_video]
 
 		self.pretrained_flownet = './pretrained/flownets_bn_EPE2.459.pth.tar'  #'None'
-		self.load_model_path = None #'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.model'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
-		self.load_optimzer_path = None #'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.optimizer'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		# Retrain
+		self.resume = False
+		self.load_model_path = 'models/v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model'.format(''.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+		self.load_optimzer_path = 'models/v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer'.format(''.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
 
-		self.record_path = 'records/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
-		self.save_model_path = 'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.model'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
-		self.save_optimzer_path = 'models/data_{}_im_{}_{}_s_{}_{}_b_{}_rnn_{}.optimizer'.format('_'.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size)
+		self.record_path = 'records/v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.txt'.format(''.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+		self.save_model_path = 'models/v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model'.format(''.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+		self.save_optimzer_path = 'models/v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer'.format(''.join(self.use_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
 
 params = Parameters()
+
