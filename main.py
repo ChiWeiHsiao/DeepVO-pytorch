3d9aa4bde4c7c0d22aec09353f272f20b8065934
ChiWeiHsiao <kiwi010379@gmail.com>
2018-06-15 15:43:16 +0200

    print std of loss means of one epoch

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model import DeepVO
 from data_helper import get_data_info, SortedRandomBatchSampler, ImageSequenceDataset
 
 
-M_deepvo = DeepVO(par.img_h, par.img_w)
+M_deepvo = DeepVO(par.img_h, par.img_w, par.batch_norm)
 use_cuda = torch.cuda.is_available()
 if use_cuda:
     print('CUDA used.')
@@ -29,15 +29,15 @@ if par.pretrained_flownet and par.load_model_path == None:
 
 
 # Prepare Data
-train_df = get_data_info(folder_list=par.train_video, seq_len_range=par.seq_len, overlap=1, sample_interval=None)
+train_df = get_data_info(folder_list=par.train_video, seq_len_range=par.seq_len, overlap=1, sample_interval=par.sample_interval)
 train_sampler = SortedRandomBatchSampler(train_df, par.batch_size, drop_last=True)
-train_dataset = ImageSequenceDataset(train_df, (par.img_w, par.img_h), par.subtract_means)
-train_dl = DataLoader(train_dataset, batch_sampler=train_sampler, num_workers=par.n_processors)
+train_dataset = ImageSequenceDataset(train_df, par.resize_mode, (par.img_w, par.img_h), par.subtract_means)
+train_dl = DataLoader(train_dataset, batch_sampler=train_sampler, num_workers=par.n_processors, pin_memory=par.pin_mem)
 
 valid_df = get_data_info(folder_list=par.valid_video, seq_len_range=par.seq_len, overlap=1, sample_interval=None)
 valid_sampler = SortedRandomBatchSampler(valid_df, par.batch_size, drop_last=True)
-valid_dataset = ImageSequenceDataset(valid_df, (par.img_w, par.img_h), par.subtract_means)
-valid_dl = DataLoader(valid_dataset, batch_sampler=valid_sampler, num_workers=par.n_processors)
+valid_dataset = ImageSequenceDataset(valid_df, par.resize_mode, (par.img_w, par.img_h), par.subtract_means)
+valid_dl = DataLoader(valid_dataset, batch_sampler=valid_sampler, num_workers=par.n_processors, pin_memory=par.pin_mem)
 
 
 
@@ -62,31 +62,48 @@ print('Record loss in: ', par.record_path)
 min_loss = 1e10
 M_deepvo.train()
 for ep in range(par.epochs):
+	st_t = time.time()
+	M_deepvo.train()
 	loss_mean = 0
-	loss_mean_valid = 0
-	
-	for it, (_, t_x, t_y) in enumerate(train_dl):
+	t_loss_list = []
+	for _, t_x, t_y in train_dl:
 		if use_cuda:
-			t_x = t_x.cuda(non_blocking=True)
-			t_y = t_y.cuda(non_blocking=True)
+			t_x = t_x.cuda(non_blocking=par.pin_mem)
+			t_y = t_y.cuda(non_blocking=par.pin_mem)
 		ls = M_deepvo.step(t_x, t_y, optimizer).data.cpu().numpy()
+		t_loss_list.append(float(ls))
+		loss_mean += float(ls)
 		if par.optim == 'Cosine':
 			lr_scheduler.step()
-		loss_mean +=  float(ls)
-	loss_mean /= (it+1)
+	print('Train loss:')
+	print('{:.1f} sec'.format(time.time()-st_t))
+	print('train stddev = {:.2f}\n'.format(np.std(t_loss_list)))
+	print(t_loss_list)
+	print('\n\n')
+	loss_mean /= len(train_dl)
 
-	for it, (_, v_x, v_y) in enumerate(valid_dl):
+	st_t = time.time()
+	M_deepvo.eval()
+	loss_mean_valid = 0
+	v_loss_list = []
+	for _, v_x, v_y in valid_dl:
 		if use_cuda:
-			v_x = v_x.cuda(non_blocking=True)
-			v_y = v_y.cuda(non_blocking=True)
-		ls = M_deepvo.get_loss(v_x, v_y).data.cpu().numpy()
-		loss_mean_valid +=  float(ls)
-	loss_mean_valid /= (it+1)
+			v_x = v_x.cuda(non_blocking=par.pin_mem)
+			v_y = v_y.cuda(non_blocking=par.pin_mem)
+		v_ls = M_deepvo.get_loss(v_x, v_y).data.cpu().numpy()
+		v_loss_list.append(float(v_ls))
+		loss_mean_valid += float(v_ls)
+	print('Valid loss:')
+	print('{:.1f} sec'.format(time.time()-st_t))
+	print('valid stddev = {:.2f}\n'.format(np.std(v_loss_list)))
+	print(v_loss_list)
+	print('\n\n')
+	loss_mean_valid /= len(valid_dl)
 
 	f = open(par.record_path, 'a')
-	f.write('Epoch {}'.format(ep+1))
-	f.write('train loss mean: {}\nvalid loss mean: {}\n\n'.format(loss_mean, loss_mean_valid))
-	print('train loss mean: {}\nvalid loss mean: {}\n\n'.format(loss_mean, loss_mean_valid))
+	f.write('Epoch {}\ntrain loss mean: {}, std: {:.2f}\nvalid loss mean: {}, std: {:.2f}\n\n'.format(ep+1, loss_mean, np.std(t_loss_list), loss_mean_valid, np.std(v_loss_list)))
+	print('Epoch {}\ntrain loss mean: {}\nvalid loss mean: {}\n\n'.format(ep+1, loss_mean, loss_mean_valid))
+	print('='*50)
 
 	# Save model
 	check_interval = 2
@@ -96,3 +113,4 @@ for ep in range(par.epochs):
 		torch.save(M_deepvo.state_dict(), par.save_model_path)
 		torch.save(optimizer.state_dict(), par.save_optimzer_path)
 	f.close()
+
