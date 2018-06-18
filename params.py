71b72a5e1aad08023d127c3fbe7ea57d950a2c14
ChiWeiHsiao <kiwi010379@gmail.com>
2018-06-18 21:59:32 +0200

    clean

--- params.py
+++ params.py
@@ -2,39 +2,27 @@ import os
 
 class Parameters():
 	def __init__(self):
-		self.n_processors = 8
+		self.n_processors = 16
 		# Path
 		self.image_dir = '/nfs/nas12.ethz.ch/fs1201/infk_ivc_students/cvg-students/chsiao/KITTI/images/'
 		self.pose_dir = '/nfs/nas12.ethz.ch/fs1201/infk_ivc_students/cvg-students/chsiao/KITTI/pose_GT/'
 
-		
-		self.train_video = ['00', '02', '08', '09', '01', '05', '07', '10']  # '06' # '04'
-		self.valid_video = [] # 06
-		self.partition = 0.8   # None # partition videos in 'train_video' to train / valid dataset
-		#self.test_video = ['06', '04']
 
-		'''
 		self.train_video = ['00', '02', '08', '09', '01', '06', ] 
 		self.valid_video = ['04', '05', '07', '10']
 		self.partition = None  # partition videos in 'train_video' to train / valid dataset
-		'''
 
-		'''
-		self.train_video = ['00', '02', '08', '09', '01', '04', '05', '06', '07', '10']
-		self.valid_video = []
-		self.partition = 0.7   # None # partition videos in 'train_video' to train / valid dataset
-		'''
 
 		# Data Preprocessing
 		self.resize_mode = 'rescale' # 'crop' 'rescale' None
-		self.img_w = 608  # 608  613  1226  1024 896
-		self.img_h = 184  # 184  185  370   312 272
-		self.img_means = (-0.14968217427134656, -0.12941663107068363, -0.1320610301921484)  #(0,0,0)
-		self.img_stds = (1, 1, 1)  #(0.309122, 0.315710, 0.3226514)  #(1, 1, 1) 
+		self.img_w = 608   #  1226
+		self.img_h = 184   #  370
+		self.img_means = (-0.14968217427134656, -0.12941663107068363, -0.1320610301921484) 
+		self.img_stds = (1, 1, 1)  #(0.309122, 0.315710, 0.3226514)
 		self.minus_point_5 = True
 
 		self.seq_len = (4, 6)  # [7, 9]
-		self.sample_times = 2  # 1
+		self.sample_times = 3  # 1
 
 		# Data info path
 		self.train_data_info_path = 'datainfo/train_df_t{}_v{}_p{}_seq{}x{}_sample{}.pickle'.format(''.join(self.train_video), ''.join(self.valid_video), self.partition, self.seq_len[0], self.seq_len[1], self.sample_times)
@@ -42,25 +30,24 @@ class Parameters():
 
 
 		# Model
-		self.rnn_hidden_size = 1000  # 1000 500
-		self.conv_dropout = (0, 0.2, 0, 0.2, 0, 0.2, 0, 0.2, 0)
+		self.rnn_hidden_size = 1000
+		self.conv_dropout = (0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1)
 		self.rnn_dropout_in = 0.5
 		self.rnn_dropout_out = 0.5
 		self.rnn_dropout_between = 0.5  # 0: no dropout
 		self.clip = None # 5
 		self.batch_norm = True
 		# Training
-		self.batch_size = 48 # 16 8
+		self.batch_size = 16
 		self.pin_mem = True
-		self.epochs = 100  #27
+		self.epochs = 100
 		self.optim = {'opt': 'Adagrad', 'lr': 0.001}
 					# {'opt': 'Adagrad', 'lr': 0.001}
 					# {'opt': 'Adam'}
 					# {'opt': 'Cosine', 'T': 100 , 'lr': 0.001}
 		
 		# Pretrain, Retrain
-		self.pretrained_flownet = None  #'./pretrained/flownets_bn_EPE2.459.pth.tar'  
-								# None
+		self.pretrained_flownet = None
 								# './pretrained/flownets_bn_EPE2.459.pth.tar'  
 								# './pretrained/flownets_EPE1.951.pth.tar'
 		self.resume = False
