64d3d856cced0cc10a3a7daebe50dc5d63a7f71a
ChiWeiHsiao <kiwi010379@gmail.com>
2018-08-07 15:42:01 +0200

    fix file name

--- preprocess.py
+++ preprocess.py
@@ -54,7 +54,8 @@ def create_pose_data():
 			lines = [line.split('\n')[0] for line in f.readlines()] 
 			poses = [ R_to_angle([float(value) for value in l.split(' ')]) for l in lines]  # list of pose (pose=list of 12 floats)
 			poses = np.array(poses)
-			np.save(fn.split('.')[0]+'.npy', poses)
+			base_fn = os.path.splitext(fn)[0]
+			np.save(base_fn+'.npy', poses)
 			print('Video {}: shape={}'.format(video, poses.shape))
 	print('elapsed time = {}'.format(time.time()-start_t))
 
